eb1a27b96cf50e15f090c8612f365c8355697c70
RobieaL <tercan_abdullah@hotmail.com>
2022-03-03 18:12:46 +0300

    sdjfsdf

--- src/main/java/org/uludag/bmb4016/App.java
+++ src/main/java/org/uludag/bmb4016/App.java
@@ -8,12 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        
-        System.out.println("Oguzhan");
-        System.out.println("batyuhan");
 
-        System.out.println("abdullah");
-        System.out.println("batuhan");
     }
 }
