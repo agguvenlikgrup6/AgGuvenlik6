637e2b49d9c6ce0120df49d25569ce2047bbeb5e
Oğuzhan Akçomak <akcomakoguzhan@gmail.com>
2022-03-05 20:52:47 +0300

    PKCE Auth v.0.1

---
+++ src/main/java/org/uludag/bmb4016/Main.java
@@ -0,0 +1,15 @@
+package org.uludag.bmb4016;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.uludag.bmb4016.auth.AuthLogin;
+
+public class Main {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        
+        
+        AuthLogin.login();
+
+    }
+}
