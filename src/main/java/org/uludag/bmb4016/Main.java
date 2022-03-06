70f92ebfc5666638dcfb3d43d2cc94837b8bbd03
Oğuzhan Akçomak <akcomakoguzhan@gmail.com>
2022-03-06 03:51:45 +0300

    javafx eklendi, readme güncellendi

--- src/main/java/org/uludag/bmb4016/Main.java
+++ src/main/java/org/uludag/bmb4016/Main.java
@@ -5,11 +5,11 @@ import java.net.URISyntaxException;
 
 import org.uludag.bmb4016.auth.AuthLogin;
 
+
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
         
-        
         AuthLogin.login();
-
+        
     }
 }
