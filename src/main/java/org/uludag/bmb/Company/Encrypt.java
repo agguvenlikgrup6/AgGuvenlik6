4357b366750908ab6f086e6df565d2112c910afb
RobieaL <tercan_abdullah@hotmail.com>
2022-03-10 23:18:57 +0300

    dosya içeriği şifreleme ve şifre çözme

--- src/main/java/org/uludag/bmb/Company/Encrypt.java
+++ src/main/java/org/uludag/bmb/Company/Encrypt.java
@@ -5,11 +5,19 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Encrypt {
+    public String textDosya = "";
     public void abc(){
         File f = new File("deneme.txt");
         try{
+            Scanner fr = new Scanner(f);
+            while(fr.hasNextLine()){
+                textDosya = fr.nextLine();
+            }
+            fr.close();
             KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
             SecretKey myDesKey = keygenerator.generateKey();
 
@@ -19,7 +27,8 @@ public class Encrypt {
             byte[] text = "No body can see me.".getBytes("UTF8");
             desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
 
-            byte[] textEncrypted = desCipher.doFinal(text);
+
+            byte[] textEncrypted = desCipher.doFinal(textDosya.getBytes(StandardCharsets.UTF_8));
             String s = new String(textEncrypted);
             System.out.println(s);
 
