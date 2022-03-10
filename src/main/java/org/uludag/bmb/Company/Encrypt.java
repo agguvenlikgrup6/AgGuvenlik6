77f39146412719e3dc92cfc08cf25db3cf38ea70
RobieaL <tercan_abdullah@hotmail.com>
2022-03-10 23:03:39 +0300

    şifreleme ve şifre çözme DES

--- src/main/java/org/uludag/bmb/Company/Encrypt.java
+++ src/main/java/org/uludag/bmb/Company/Encrypt.java
@@ -1,24 +1,47 @@
 package org.uludag.bmb.Company;
 
 import java.io.File;
-import java.io.IOException;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import java.io.FileReader;
 
 public class Encrypt {
     public void abc(){
         File f = new File("deneme.txt");
         try{
-            FileReader fr = new FileReader(f);
-            int c = fr.read();
-            while(c != -1){
-                char k = (char)c;
-                System.out.println(k + " ");
-                c = fr.read();
-            }
-            fr.close();
+            KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
+            SecretKey myDesKey = keygenerator.generateKey();
+
+            Cipher desCipher;
+            desCipher = Cipher.getInstance("DES");
+
+            byte[] text = "No body can see me.".getBytes("UTF8");
+            desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
+
+            byte[] textEncrypted = desCipher.doFinal(text);
+            String s = new String(textEncrypted);
+            System.out.println(s);
+
+            desCipher.init(Cipher.DECRYPT_MODE, myDesKey);
+            byte[] textDecrypted = desCipher.doFinal(textEncrypted);
+
+            s = new String(textDecrypted);
+            System.out.println(s);
+
+
+            // FileReader fr = new FileReader(f);
+            // int c = fr.read();
+            // while(c != -1){
+            //     char k = (char)c;
+            //     System.out.println(k + " ");
+            //     c = fr.read();
+            // }
+            // fr.close();
         }
         catch(Exception e){
-            e.printStackTrace();
+            //e.printStackTrace();
+            System.out.println("Exception");
         }
     }
 
