476f8e6939eff12057d17694c803690714f9c863
Oğuzhan Akçomak <akcomakoguzhan@gmail.com>
2022-03-12 21:28:42 +0300

    fxml css eklenebiliyor.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,12 @@
-module org.uludag.bmb.gui {
+module org.uludag.bmb{
     requires javafx.controls;
     requires javafx.fxml;
     requires java.logging;
     requires java.desktop;
     requires dropbox.core.sdk;
+    requires javafx.web;
     requires transitive javafx.graphics;
-    
-    opens org.uludag.bmb.gui to javafx.fxml;
+
+    opens org.uludag.bmb.gui to javafx.fxml, javafx.controls, javafx.base, javafx.graphics;
     exports org.uludag.bmb.gui;
 }
