d6d6691852756f2b1ae46e28ddec10413eed38ba
RobieaL <tercan_abdullah@hotmail.com>
2022-03-22 21:09:35 +0300

    .

--- src/main/java/org/uludag/bmb/Main.java
+++ src/main/java/org/uludag/bmb/Main.java
@@ -3,17 +3,14 @@ package org.uludag.bmb;
 import java.net.URISyntaxException;
 import java.io.IOException;
 
-import org.uludag.bmb.Company.Encrypt;
-import org.uludag.bmb.Company.EncryptNew;
 import org.uludag.bmb.Company.FileSystem;
 
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
         try{
             FileSystem tt = new FileSystem();
-            EncryptNew aa = new EncryptNew();
-            tt.dosyaSifreleme("deneme.txt", "aa.txt");
-            tt.dosyaSifreCozme("aa.txt","dd.txt");
+            //tt.dosyaSifreleme("deneme.txt", "aa.txt");
+            //tt.dosyaSifreCozme("aa.txt","dd.txt");
         }
         catch(Exception ignored){
             
