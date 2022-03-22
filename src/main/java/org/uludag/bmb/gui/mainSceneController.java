cfe1da3f88b60e43dd76fcd5ebc29bae6b001bb3
BATUHAN SUNAR <79984606+krokennaz@users.noreply.github.com>
2022-03-23 02:27:10 +0300

    deneme

--- src/main/java/org/uludag/bmb/gui/mainSceneController.java
+++ src/main/java/org/uludag/bmb/gui/mainSceneController.java
@@ -1,11 +1,27 @@
 package org.uludag.bmb.gui;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javax.swing.JFileChooser;
-import javax.swing.JFrame;
+
+import com.dropbox.core.DbxDownloader;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
+
+import org.uludag.bmb.entity.*;
+import org.uludag.bmb.oauth.DbxClientLogin;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,37 +36,10 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javax.swing.*;
-import java.awt.event.*;
-import java.awt.*;
-import java.util.*;
 
-public class mainSceneController implements Initializable{
+public class mainSceneController extends DbxClientLogin implements Initializable {
+    String path;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
-        TreeItem<String> rootItem = new TreeItem<>("Files");
-        
-        TreeItem<String> branchItem1 = new TreeItem<>("aaa");
-        TreeItem<String> branchItem2 = new TreeItem<>("bbb");
-        TreeItem<String> branchItem3 = new TreeItem<>("ccc");
-
-
-        TreeItem<String> leafItem1 = new TreeItem<>("aa");
-        TreeItem<String> leafItem2 = new TreeItem<>("bb");
-        TreeItem<String> leafItem3 = new TreeItem<>("cc");
-        
-        branchItem1.getChildren().add(leafItem1);
-        branchItem2.getChildren().add(leafItem2);
-        branchItem3.getChildren().add(leafItem3);
-       
-        rootItem.getChildren().addAll(branchItem1,branchItem2,branchItem3);
-
-        treeView.setRoot(rootItem);
-
-    }
-        
     @FXML
     private Button btnDownload;
 
@@ -59,9 +48,12 @@ public class mainSceneController implements Initializable{
 
     @FXML
     private TreeView treeView;
-    
+
     @FXML
     private Text files;
+
+    @FXML
+    private TreeView showFiles;
 
     @FXML
     private Font x1;
@@ -75,61 +67,127 @@ public class mainSceneController implements Initializable{
     @FXML
     private Color x4;
 
-    @FXML
-    void selectItem(MouseEvent event) {
-        TreeItem<String> item =(TreeItem<String>) treeView.getSelectionModel().getSelectedItem();
-        if(item.getValue()!=null){
-            System.out.println(item.getValue());
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        MXMTree hierarchy = new DBHierarchy().getHierarchy();
+        MXMNode hierarchyRoot = hierarchy.root;
+
+        TreeItem<String> rootItem = new TreeItem<>(hierarchyRoot.data);
+
+        treeView.setRoot(rootItem);
+
+        addtotree(rootItem, hierarchyRoot);
+
+    }
+
+    private void addtotree(TreeItem<String> rootItem, MXMNode hierarchyRoot) {
+        if (hierarchyRoot.childs.size() != 0) {
+            int i = 0;
+            for (MXMNode c : hierarchyRoot.childs) {
+                hierarchyRoot = c;
+                rootItem.getChildren().add(new TreeItem<>(hierarchyRoot.data));
+                addtotree(rootItem.getChildren().get(i++), hierarchyRoot);
+            }
+        } else {
+            for (MXMNode c : hierarchyRoot.leafs) {
+                rootItem.getChildren().add(new TreeItem<>(c.data));
+            }
         }
     }
+
     @FXML
-    void downloadItem(ActionEvent event) {
-        JFileChooser chooser ;
-        
-        chooser = new JFileChooser(); 
-        chooser.showSaveDialog(null);
-        chooser.setCurrentDirectory(new java.io.File("."));
-        chooser.setDialogTitle("choosertitle");
-        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        //
-        // disable the "All files" option.
-        //
-        chooser.setAcceptAllFileFilterUsed(false);
-        System.out.println(chooser.getCurrentDirectory());
-        // JFrame frame = new JFrame("");
-        // DemoJFileChooser panel = new DemoJFileChooser();
-        // frame.addWindowListener(
-        //   new WindowAdapter() {
-        //     public void windowClosing(WindowEvent e) {
-        //       System.exit(0);
-        //       }
-        //     }
-        //   );
-        // frame.getContentPane().add(panel,"Center");
-        // frame.setSize(panel.getPreferredSize());
-        // frame.setVisible(true);
-        // System.out.println("aasas");
-        // Download download=new Download();
+    void downloadFile(MouseEvent event) {
 
     }
 
-   
+    @FXML
+    void selectItem(MouseEvent event) {
+        TreeItem<String> item = (TreeItem<String>) treeView.getSelectionModel().getSelectedItem();
+
+        try {
+            ArrayList<String> pathList = new ArrayList<>();
+            path = "";
+            var fake = item;
+
+            if (item.getParent() == null) {
+                path += "/";
+            } else {
+
+                while (fake.getParent() != null) {
+                    pathList.add("/");
+                    pathList.add(fake.getValue());
+                    fake = fake.getParent();
+                }
+
+                pathList.add("/");
+                Collections.reverse(pathList);
+            }
+
+            for (String p : pathList) {
+                path += p;
+            }
+
+            ListFolderResult result = client.files().listFolder(path);
+
+            List<Metadata> entries = result.getEntries();
+
+            TreeItem<String> treeitem = new TreeItem<>("AAAA");
+            showFiles.setRoot(treeitem);
+
+            for (Metadata metadata : entries) {
+                if (metadata instanceof FileMetadata) {
+                    treeitem.getChildren().add(new TreeItem<>(metadata.getName()));
+                }
+            }
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    @FXML
+    void downloadItem(ActionEvent event) {
+        TreeItem<String> item = (TreeItem<String>) showFiles.getSelectionModel().getSelectedItem();
+
+        try {
+            path += item.getValue();
+
+            DbxDownloader<FileMetadata> downloader = client.files().download(path);
+
+            FileOutputStream out = new FileOutputStream(item.getValue());
+            downloader.download(out);
+            out.close();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
 
     @FXML
     void uploadItem(ActionEvent event) throws IOException {
-        JFileChooser fileChooser=new JFileChooser();
+        JFileChooser fileChooser = new JFileChooser();
         fileChooser.showOpenDialog(null);
-        
+
         System.out.println(fileChooser.getSelectedFile().toPath().toString());
-        String path=fileChooser.getSelectedFile().toPath().toString();
-        UploadFile uploadFile=new UploadFile();
-        uploadFile.uploadFileFunc(path);
+        String fileName = fileChooser.getSelectedFile().getName();
+        System.out.println(fileName);
+        String uploadFilePath = fileChooser.getSelectedFile().toPath().toString();
+        // UploadFile uploadFile = new UploadFile();
+        // uploadFile.uploadFileFunc(uploadFilePath);
+
+        try {
+            try (InputStream in = new FileInputStream(uploadFilePath)) {
+                client.files().uploadBuilder(path + fileName).uploadAndFinish(in);
+            }
+        } catch (DbxException exception) {
+            System.err.println(exception.getMessage());
+        }
     }
-    
+
     @FXML
     void getPath(MouseEvent event) throws IOException {
         System.out.println(files.getText());
-       
+
         // download.downloadFile(files.getText());
     }
 
