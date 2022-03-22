9fc78c2f338fe07224d1df146ae294d1e451bd0d
BATUHAN SUNAR <79984606+krokennaz@users.noreply.github.com>
2022-03-22 22:07:50 +0300

    login fxml ve cont eklendi

--- src/main/java/org/uludag/bmb/gui/App.java
+++ src/main/java/org/uludag/bmb/gui/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("mainScene"));
+        scene = new Scene(loadFXML("loginScene"));
         stage.setScene(scene);
         stage.show();
     }
