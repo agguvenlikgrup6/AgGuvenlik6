68c4931391eb3f72211ada7fdeca3cec02038ec5
RobieaL <tercan_abdullah@hotmail.com>
2022-03-26 22:18:11 +0300

    RSA simple encryption and decryption

--- src/main/java/org/uludag/bmb/Main.java
+++ src/main/java/org/uludag/bmb/Main.java
@@ -5,13 +5,21 @@ import java.io.File;
 import java.io.IOException;
 
 import org.uludag.bmb.Company.FileSystem;
+import org.uludag.bmb.Company.TryRSA;
 
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
         try{
-            FileSystem tt = new FileSystem();
+            //FileSystem tt = new FileSystem();
             //tt.dosyaSifreleme("deneme.txt", "aa.txt");
             //tt.dosyaSifreCozme("aa.txt","dd.txt");
+            TryRSA rsa = new TryRSA();
+            rsa.init();
+            String encryptedMessage = rsa.encrypt("Hello World!");
+            String decryptedMessage = rsa.decrypt(encryptedMessage);
+
+            System.out.println(encryptedMessage);
+            System.out.println(decryptedMessage);
         }
         catch(Exception ignored){
             
