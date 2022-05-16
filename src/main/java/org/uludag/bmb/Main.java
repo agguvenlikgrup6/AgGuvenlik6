e07547368cd3ef3e981b5858926d9b9a671bbbcc
RobieaL <tercan_abdullah@hotmail.com>
2022-05-16 14:22:11 +0300

    Collection iki liste arası farklı elemanlar

--- src/main/java/org/uludag/bmb/Main.java
+++ src/main/java/org/uludag/bmb/Main.java
@@ -3,7 +3,12 @@ package org.uludag.bmb;
 import java.net.URISyntaxException;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Collection;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,10 +21,24 @@ import org.uludag.bmb.Company.TryRSA;
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
         try{
-            File a = new File("görselprog.pdf");
-            byte[] abc = Files.readAllBytes(a.toPath());
-            HashTry pp = new HashTry();
-            pp.getHash(abc);
+            // File a = new File("görselprog.pdf");
+            // byte[] abc = Files.readAllBytes(a.toPath());
+            // HashTry pp = new HashTry();
+            // pp.getHash(abc);
+            Collection<String> listOne = new ArrayList<>(Arrays.asList("milan","dingo","elpha","hafil","meat","iga","neeta.peeta"));
+            Collection<String> listTwo = new ArrayList<>(Arrays.asList("hafil","iga","binga","mike","dingo"));
+            Collection<String> similar = new HashSet<String>(listOne);
+            Collection<String> different = new HashSet<String>();
+            different.addAll(listOne);
+            different.addAll(listTwo);
+            similar.retainAll(listTwo);
+            different.removeAll(similar);
+            System.out.println("1-->  " + listOne);
+            System.out.println("2-->  " + listTwo);
+            System.out.println("similar-->  " + similar);
+            System.out.println("different-->  " + different);
+
+
             //Crypto tt = new Crypto();
             //tt.dosyaSifrelemeAll("deneme.txt");
             //tt.dosyaAdSifreCozme("hwP5+EixvLljXHFsDJTERM3FeIaF74LTqAw=");
