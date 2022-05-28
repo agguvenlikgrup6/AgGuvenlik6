6441a4b70a1ed373299a97a7d63ff6509e4cd009
BATUHAN SUNAR <79984606+krokennaz@users.noreply.github.com>
2022-05-28 03:56:41 +0300

    shared

---
+++ src/main/java/org/uludag/bmb/controller/scene/selectShareFolderSceneController.java
@@ -0,0 +1,32 @@
+package org.uludag.bmb.controller.scene;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.uludag.bmb.operations.scenedatasource.UITrees;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
+public class selectShareFolderSceneController {
+
+    @FXML
+    private Button saveSharedToFolderBtn;
+
+    @FXML
+    private TreeView<String> selectShareFolderTreeView;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        TreeItem<String> root = UITrees.Hierarchy.getAsTreeItem("");
+        selectShareFolderTreeView.setRoot((TreeItem<String>) root);
+        selectShareFolderTreeView.setShowRoot(false);
+    }
+    @FXML
+    void saveSharedToFolder(ActionEvent event) {
+
+    }
+
+}
